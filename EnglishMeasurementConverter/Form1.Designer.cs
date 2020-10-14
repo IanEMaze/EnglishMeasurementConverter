8bcbc52708e84b7e2d1a0c10d4f8b5781e115ac9
IanEMaze <65833998+IanEMaze@users.noreply.github.com>
2020-10-14 11:17:40 -0400

    Add files via upload

---
+++ EnglishMeasurementConverter/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace EnglishMeasurementConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.userInputBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fromListBox = new System.Windows.Forms.ListBox();
+            this.toListBox = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.userOutputBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(108, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a distance to convert:";
+            // 
+            // userInputBox
+            // 
+            this.userInputBox.Location = new System.Drawing.Point(322, 41);
+            this.userInputBox.Name = "userInputBox";
+            this.userInputBox.Size = new System.Drawing.Size(186, 20);
+            this.userInputBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(92, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(427, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "This application will convert user intput into the desired measurment system";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 45);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "From:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(423, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 45);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "To:";
+            // 
+            // fromListBox
+            // 
+            this.fromListBox.FormattingEnabled = true;
+            this.fromListBox.Items.AddRange(new object[] {
+            "Inches",
+            "Feet",
+            "Yards"});
+            this.fromListBox.Location = new System.Drawing.Point(95, 138);
+            this.fromListBox.Name = "fromListBox";
+            this.fromListBox.Size = new System.Drawing.Size(125, 43);
+            this.fromListBox.TabIndex = 5;
+            // 
+            // toListBox
+            // 
+            this.toListBox.FormattingEnabled = true;
+            this.toListBox.Items.AddRange(new object[] {
+            "Inches",
+            "Feet",
+            "Yards"});
+            this.toListBox.Location = new System.Drawing.Point(394, 138);
+            this.toListBox.Name = "toListBox";
+            this.toListBox.Size = new System.Drawing.Size(125, 43);
+            this.toListBox.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(135, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Converted Distance:";
+            // 
+            // userOutputBox
+            // 
+            this.userOutputBox.Location = new System.Drawing.Point(341, 267);
+            this.userOutputBox.Name = "userOutputBox";
+            this.userOutputBox.Size = new System.Drawing.Size(133, 20);
+            this.userOutputBox.TabIndex = 8;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.Location = new System.Drawing.Point(264, 188);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(93, 44);
+            this.convertButton.TabIndex = 9;
+            this.convertButton.Text = "Convert!";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(12, 310);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(94, 47);
+            this.resetButton.TabIndex = 10;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(509, 310);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(94, 47);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Impact", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(426, 342);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "(Alt + X to close)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(615, 369);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.userOutputBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.toListBox);
+            this.Controls.Add(this.fromListBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userInputBox);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "English Distance Conversion App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userInputBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox fromListBox;
+        private System.Windows.Forms.ListBox toListBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox userOutputBox;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ EnglishMeasurementConverter/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnglishMeasurementConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //Exit button will close the form or window
+            this.Close();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            //Reset button will clear all controls that can be changed by the user
+            userInputBox.Text = "";
+            userOutputBox.Text = "";
+            fromListBox.ClearSelected();
+            toListBox.ClearSelected();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            //Starting with the try/catch to display any possible errors to user
+            try
+            {
+                //Creating variables
+                double Number, Total = 0;
+
+                //Setting Focus for user input
+                userInputBox.Focus();
+
+                //Input Validation
+                if (!(double.TryParse(userInputBox.Text, out Number)))
+                {
+                    MessageBox.Show("Please input a valid number, Try again.");
+                }
+
+                //Math Time! (Inches to Yards)
+                if (fromListBox.Text == "Inches" && toListBox.Text == "Yards") {
+                    Total = Number / 36;
+                    userOutputBox.Text = Total.ToString("f1") + " " + toListBox.Text;
+                }
+
+                //Math Time! (Inches to Feet)
+                if (fromListBox.Text == "Inches" && toListBox.Text == "Feet")
+                {
+                    Total = Number / 12;
+                    userOutputBox.Text = Total.ToString("f1") + " " + toListBox.Text;
+                }
+
+                //Math Time! (Feet to Yards)
+                if (fromListBox.Text == "Feet" && toListBox.Text == "Yards")
+                {
+                    Total = Number / 3;
+                    userOutputBox.Text = Total.ToString("f1") + " " + toListBox.Text;
+                }
+
+                //Math Time! (Feet to Inches)
+                if (fromListBox.Text == "Feet" && toListBox.Text == "Inches")
+                {
+                    Total = Number * 12;
+                    userOutputBox.Text = Total.ToString("f1") + " " + toListBox.Text;
+                }
+
+                //Math Time! (Yards to Inches)
+                if (fromListBox.Text == "Yards" && toListBox.Text == "Inches")
+                {
+                    Total = Number * 36;
+                    userOutputBox.Text = Total.ToString("f1") + " " + toListBox.Text;
+                }
+                //Math Time! (Yards to Feet)
+                if (fromListBox.Text == "Yards" && toListBox.Text == "Feet")
+                {
+                    Total = Number * 3;
+                    userOutputBox.Text = Total.ToString("f1") + " " + toListBox.Text;
+                }
+
+                //Math Time! (Same FROM and TO selected)
+                if (fromListBox.SelectedItem == toListBox.SelectedItem)
+                {
+                    userOutputBox.Text = Number + " " + fromListBox.Text;
+                }
+            }
+
+            //Catch will display any possible errors with program
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
